37a390e8ba097a7c856bebd26a9b155968bd6f60
phong.bnh <phong.bnh@globalfactories.com>
2023-08-23 22:01:04 +0700

    update user,login,signup dtos and jwt settings

---
+++ Services/Mango.Services.AuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.AuthAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Auth API");
+    }
+    
+    [HttpPost]
+    public IActionResult Post()
+    {
+        return Ok("Auth API");
+    }
+}
