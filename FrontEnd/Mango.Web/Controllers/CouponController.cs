4928088f133c230433061ed339cfdda168dc10d5
phong.bnh <phong.bnh@globalfactories.com>
2023-08-23 17:57:11 +0700

    WIP

---
+++ FrontEnd/Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers;
+
+public class CouponController : Controller
+{
+    private readonly ICouponService _couponService;
+    
+    public CouponController(ICouponService couponService)
+    {
+        _couponService = couponService;
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        List<CouponDto>? list = new();
+        
+        var response = await _couponService.GetCouponsAsync();
+        
+        if (response is not null && response.IsSuccess)
+        {
+            list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+        }
+        
+        return View(list);
+    }
+}
