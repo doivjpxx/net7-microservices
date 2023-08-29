1712058547e5c8ecff75eec190ee3567e6a7bbfa
phong.bnh <phong.bnh@globalfactories.com>
2023-08-29 19:58:44 +0700

    feat: add validation for model

--- FrontEnd/Mango.Web/Controllers/HomeController.cs
+++ FrontEnd/Mango.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Mango.Web.Models;
+using Mango.Web.Utils;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Mango.Web.Controllers;
 
@@ -18,6 +20,7 @@ public class HomeController : Controller
         return View();
     }
 
+    [Authorize(Roles = SD.RoleAdmin)]
     public IActionResult Privacy()
     {
         return View();
