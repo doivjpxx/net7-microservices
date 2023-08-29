ad788a290bb7a90c4d59aad433daffb05f5d71f0
phong.bnh <phong.bnh@globalfactories.com>
2023-08-29 20:38:23 +0700

    feat: split authentication into programextension

--- Services/Mango.Services.CouponAPI/Program.cs
+++ Services/Mango.Services.CouponAPI/Program.cs
@@ -1,10 +1,9 @@
-using System.Text;
 using AutoMapper;
 using Mango.Services.CouponAPI;
 using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -49,28 +48,8 @@ builder.Services.AddSwaggerGen(option =>
     });
 });
 
-var secret = builder.Configuration.GetValue<string>("ApiSettings:Secret");
-var issuer = builder.Configuration.GetValue<string>("ApiSettings:Issuer");
-var audience = builder.Configuration.GetValue<string>("ApiSettings:Audience");
-
-var key = Encoding.UTF8.GetBytes(secret);
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(o =>
-{
-    o.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = audience,
-        ValidIssuer = issuer,
-    };
-});
+// From extensions method
+builder.AddAppAuthentication();
 
 builder.Services.AddAuthorization();
 
