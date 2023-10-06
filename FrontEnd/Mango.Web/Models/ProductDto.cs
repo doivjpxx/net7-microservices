88ef66d4b68ce164659134cb90b7a742ff87b4af
phong.bnh <phong.bnh@globalfactories.com>
2023-10-06 15:48:04 +0700

    feat: update home page in razor page

--- FrontEnd/Mango.Web/Models/ProductDto.cs
+++ FrontEnd/Mango.Web/Models/ProductDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Mango.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models;
 
 public class ProductDto
 {
@@ -8,4 +10,6 @@ public class ProductDto
     public string Description { get; set; } = string.Empty;
     public string CategoryName { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
+    [Range(1,100)]
+    public int Count { get; set; } = 1;
 }
